05e545fe5b844af5e4000a2e4736b1289c10ca05
OfentseXI <dofentse69@gmail.com>
2024-10-22 16:25:10 +0200

    Remove grid-cols-1 and md:grid-cols-4 classes from container div

--- components/RecipeGrid.jsx
+++ components/RecipeGrid.jsx
@@ -59,7 +59,7 @@ export default function RecipeGrid() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+      <div>
         <div className="md:col-span-3">
           {loading ? (
             <div className="flex justify-center items-center min-h-[400px]">
