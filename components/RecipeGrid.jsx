b7b9bcbedcc7b03795484177bc9aae6771ba29d4
OfentseXI <dofentse69@gmail.com>
2024-10-22 16:33:01 +0200

    Update grid layout in RecipeGrid component

--- components/RecipeGrid.jsx
+++ components/RecipeGrid.jsx
@@ -67,7 +67,7 @@ export default function RecipeGrid() {
             </div>
           ) : (
             <>
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6">
                 {recipes.map((recipe) => (
                   <RecipeCard key={recipe._id} recipe={recipe} />
                 ))}
