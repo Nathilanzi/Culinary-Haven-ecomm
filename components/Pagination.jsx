ca5a0ef44bd41dcfdc45d96b01f521402a975f9f
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-10-22 13:22:29 +0200

    Add props to Pagination component and basic navigation structure

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-export function Pagination() {
+export function Pagination({
+  currentPage,
+  totalPages,
+  onPageChange,
+  isLastPage,
+}) {
   return (
-    <div>
-        {/* Pagination component */}
+    <div className="flex justify-center items-center space-x-2 my-8">
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
+      <span>{currentPage}</span>
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={isLastPage}
+      >
+        Next
+      </button>
     </div>
-    );
+  );
 }
