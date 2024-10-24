5505a71e6dd004d394c0e3be5e52a95ce73c4fbe
OfentseXI <dofentse69@gmail.com>
2024-10-24 13:49:21 +0200

    Add loading spinner to Gallery component

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -8,6 +8,7 @@ const Gallery = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState(0);
   const [animation, setAnimation] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (animation) {
@@ -36,6 +37,31 @@ const Gallery = ({ images }) => {
     <div className="relative overflow-hidden shadow-md group">
       {/* Set the height for uniformity */}
       <div className="w-full h-[15rem]">
+      {isLoading && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            {/* Loading spinner */}
+            <svg
+              className="animate-spin h-8 w-8 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        )}
         {images.map((image, index) => (
           <div
             key={index}
