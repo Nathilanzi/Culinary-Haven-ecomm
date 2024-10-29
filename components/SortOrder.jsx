05d0d5e5848ae80d4a77fe7eba8dec0df2a10954
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-10-29 14:47:32 +0200

    Add useRouter and useSearchParams hooks for navigation handling in SortOrder component

--- components/SortOrder.jsx
+++ components/SortOrder.jsx
@@ -1,6 +1,10 @@
 "use client";
+
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default function SortOrder({ currentSort, currentOrder }) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   return <div>Sort Order Component</div>;
 }
