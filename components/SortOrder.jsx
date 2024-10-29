3b083547141b32f2872244d9a8abcfd608105964
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-10-29 14:48:52 +0200

    Implement handleSortChange function to manage sort and order URL parameters

--- components/SortOrder.jsx
+++ components/SortOrder.jsx
@@ -6,5 +6,16 @@ export default function SortOrder({ currentSort, currentOrder }) {
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  const handleSortChange = (e) => {
+    const [sortBy, order] = e.target.value.split("-");
+    const params = new URLSearchParams(searchParams);
+
+    params.set("sortBy", sortBy);
+    params.set("order", order);
+    params.delete("page");
+    router.push(`/?${params.toString()}`);
+  };
+
+
   return <div>Sort Order Component</div>;
 }
