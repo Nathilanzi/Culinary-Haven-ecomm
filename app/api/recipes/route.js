0859efdfceced440a2bd642133529eb18738f174
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-10-29 09:53:33 +0200

    Add sorting logic

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -14,6 +14,8 @@ export async function GET(request) {
     const limit = parseInt(searchParams.get("limit")) || 20;
     const search = searchParams.get("search");
     const category = searchParams.get("category");
+    const sortBy = searchParams.get("sortBy") || "title";
+    const order = searchParams.get("order") || "asc";
 
     // Connect to MongoDB
     const client = await clientPromise;
@@ -34,12 +36,14 @@ export async function GET(request) {
 
     // Calculate number of documents to skip
     const skip = (page - 1) * limit;
+    const sortObject = { [sortBy]: order === "asc" ? 1 : -1};
 
     const [recipes, total, categories] = await Promise.all([
       db
         .collection("recipes")
         .find(query)
         .skip(skip)
+        .sort(sortObject)
         .limit(limit)
         .toArray(),
       db.collection("recipes").countDocuments(query),
