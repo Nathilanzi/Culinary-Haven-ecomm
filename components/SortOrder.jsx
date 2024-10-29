4f24c7e1e04e06679abe83ee792a5fac60d2ac7d
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-10-29 14:46:34 +0200

    Initialize SortOrder component with client-side rendering setup

---
+++ components/SortOrder.jsx
@@ -0,0 +1,6 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function SortOrder({ currentSort, currentOrder }) {
+  return <div>Sort Order Component</div>;
+}
