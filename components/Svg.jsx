0147df8a84d1e6bb123d716cab1ed504b48cf1bd
ChristopherTsh <christophertshoma0045@gmail.com>
2024-11-21 13:28:05 +0200

    Add JSDoc comments to functions in route.js and page.jsx, extracted SearchIcon component to Svg.jsx, and refactored code in page.jsx and route.js

--- components/Svg.jsx
+++ components/Svg.jsx
@@ -292,6 +292,28 @@ export const NutritionIcon = () => (
 );
 
 /**
+ * SVG icon for search representation.
+ * @constant
+ * @type {React.FC<React.SVGProps<SVGSVGElement>>}
+ */
+export const SearchIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="11" cy="11" r="8" />
+    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+  </svg>
+);
+
+/**
  * Object containing SVG icons for common allergens.
  * Keys represent allergen names, and values are corresponding SVG icons.
  * @constant
