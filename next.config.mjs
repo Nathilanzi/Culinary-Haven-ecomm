3d483aa1047ebe0c551a94fa157bdd4193767de2
Rethabile <rethadiale27@gmail.com>
2024-11-25 13:07:38 +0200

    Updated next.config.mjs to include PWA configuration for improved functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -18,4 +20,11 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
+
+export default pwaConfig;
